c75a4851cc63c6c80448d30a6be6a9ecef82fca8
sakshi26072001 <74151176+sakshi26072001@users.noreply.github.com>
2022-07-23 12:11:24 +0530

    sdjk

---
+++ t1.py
@@ -0,0 +1,16 @@
+from turtle import * # this is library (i.e turtle)
+forward(200) #move forward 200 units
+left(90) #turn left 90 degrees
+forward(100)
+left(90)
+forward(200)
+left(120)
+forward(100)
+right(360)
+left(360)
+mainloop()  #stops the window from closing
+
+
+
+
+
